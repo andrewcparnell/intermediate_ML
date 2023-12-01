da870866c2524b4db702629109a5f7f40922902f
andrewcparnell <andrew.parnell@gmail.com>
2023-12-01 14:39:37 +0000

    Update practical 1

--- scripts/cifar10_keras_cnn.R
+++ scripts/cifar10_keras_cnn.R
@@ -38,7 +38,8 @@ y_test <- to_categorical(y_test, 10)
 
 # 4. Build the CNN model
 model <- keras_model_sequential() %>%
-  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu', input_shape = c(32, 32, 3)) %>%
+  layer_conv_2d(filters = 32, kernel_size = c(3,3), activation = 'relu', 
+                input_shape = c(32, 32, 3)) %>%
   layer_max_pooling_2d(pool_size = c(2,2)) %>%
   layer_conv_2d(filters = 64, kernel_size = c(3,3), activation = 'relu') %>%
   layer_max_pooling_2d(pool_size = c(2,2)) %>%
